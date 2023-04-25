511f0c80d2915daef6749ef7f3fa2aee24a68bf5
Shulamis Friedler <12shulamis@gmail.com>
2023-04-25 17:44:56 -0500

    Add project files.

---
+++ MyFirstGitRepo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstGitRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
